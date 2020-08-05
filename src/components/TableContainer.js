b24f7501f492e711e8a6908488ad5faba0bd8619
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-05 11:41:23 +0100

    Created component placeholders

---
+++ src/components/TableContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class TableContainer extends Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default TableContainer;

4018049d62821833d298c58e9e6ee6a3ba55759f
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-05 12:18:44 +0100

    Wrote the JSX for the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,17 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
-
+      <div style={{width: "100%", color: "white", "text-align": "center", "font-family": "'Raleway', sans-serif"}}>
+        <div style={{"background-color": "black", height: "125px", padding: "20px"}}>
+          <h1>
+            Employee Directory
+          </h1>
+          <p>
+            Click on the carrots to filter by heading or use the search box to narrow your results
+          </p>
+        </div>
+        <div style={{"background-color": "red", height: "5px"}}>
+        </div>
       </div>
     );
   }

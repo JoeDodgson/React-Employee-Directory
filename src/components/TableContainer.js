8ab4ce93239a67a1ea467f08ac031edd1859a4e6
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-05 14:58:04 +0100

    Created JSX for table container

--- src/components/TableContainer.js
+++ src/components/TableContainer.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import TableHeader from "./TableHeader"
+import TableResults from "./TableResults";
 
 class TableContainer extends Component {
-
+  
   render() {
     return (
       <div>
-
+        <table className="table table-striped" style={{textAlign: "center"}}>
+          <TableHeader></TableHeader>
+          <tbody>
+            <TableResults></TableResults>
+          </tbody>
+        </table>
       </div>
     );
   }

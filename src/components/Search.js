17df5b1500b3fb11b52ad11cc5fa156fecd97ab2
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-06 09:11:53 +0100

    Call handleInputChange when text entered into search
    -Changed the value of the search input based on props.search
    -Changed the name of the input to 'search'

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,7 +7,9 @@ class Search extends Component {
       <div style={{backgroundColor: "#e9ecef", height: "100px", textAlign: "center"}}>
         <form style={{paddingTop: "30px"}}>
           <input
-            name="filter"
+            value={this.props.search}
+            name="search"
+            onChange={this.props.handleInputChange}
             type="text"
             placeholder="Search"
           />

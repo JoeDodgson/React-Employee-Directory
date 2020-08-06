76c644ba1ac7b8b386caeaecf79f87e8c4f175ff
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-06 09:10:11 +0100

    Added component logic to App.js
    -Changed app to React component class
    -Initialised state
    -Added a handleInputChange event which sets the state
    -Passed search and handleInputChange into children component props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import Header from './components/Header.js';
 import Search from './components/Search.js';
@@ -7,17 +7,32 @@ import TableHeader from './components/TableHeader.js';
 import TableResults from './components/TableResults.js';
 import './App.css';
 
-function App() {
-  return (
-    <div>      
-      <Header></Header>
-      <Search></Search>
-      <TableContainer>
-        <TableHeader></TableHeader>
-        <TableResults></TableResults>
-      </TableContainer>
-    </div>
-  );
+class App extends Component {
+  // Set the component's initial state
+  state = {
+    search: ""
+  };
+
+  handleInputChange = event => {
+    // Get the value and name of the input which triggered the change
+    let { value, name } = event.target;
+
+    // Update the input's state
+    this.setState({
+      [name]: value.toLowerCase()
+    });
+  };
+
+  render() {
+    return (
+      <div>      
+        <Header></Header>
+        <Search search={this.state.search} handleInputChange={this.handleInputChange}></Search>
+        <TableContainer search={this.state.search}>
+        </TableContainer>
+      </div>
+    )
+  }
 }
 
 export default App;

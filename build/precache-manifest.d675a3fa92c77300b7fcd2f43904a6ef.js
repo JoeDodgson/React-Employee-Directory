41fb910578999f1fba4870d86ee4cdaa2773d2d1
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-08 19:35:17 +0100

    Ran deployment and build scripts to host on Github pages

---
+++ build/precache-manifest.d675a3fa92c77300b7fcd2f43904a6ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff94517f2479c6ff1bf959e1e080e17",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "547b91b249514e24ec13",
+    "url": "/React-Employee-Directory/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "bc183dca8989e5f2e00a",
+    "url": "/React-Employee-Directory/static/js/2.cc709ac6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.cc709ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547b91b249514e24ec13",
+    "url": "/React-Employee-Directory/static/js/main.445787de.chunk.js"
+  },
+  {
+    "revision": "d5a550fc769b1b6308d1",
+    "url": "/React-Employee-Directory/static/js/runtime-main.f187368d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/React-Employee-Directory/static/media/logo.ee7cd8ed.svg"
+  }
+]);
